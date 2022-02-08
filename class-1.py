707e0a5d77181a62343be9d8a8d743b7a2f2abac
avdsm <aravoskan@gmail.com>
2022-02-08 17:38:19 +0400

    Posle prvogo uroka

--- class-1.py
+++ class-1.py
@@ -1,6 +1,17 @@
 # print("Hello World")
 # print("22222")
 
-print(10 + 15)
-print(4 ** 2)
-print(25 // 2)
+# print(10 + 15)
+# print(4 ** 2)
+# print(25 // 2)
+
+# print("Hello" + "World")
+
+# a = "1"
+# b = "5"
+# c = 3
+# d = a + b
+# print(int(d) + c)
+
+c = 3.14568
+print(round(c, 2))
