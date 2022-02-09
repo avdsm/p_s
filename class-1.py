d402f1f0f3a47ccdac51f2827b899f4e74f494d1
avdsm <aravoskan@gmail.com>
2022-02-09 12:21:35 +0400

    Home Work 1

--- class-1.py
+++ class-1.py
@@ -16,5 +16,17 @@
 # c = 3.14568
 # print(round(c, 2))
 
-name = input("Please enter you name: ")
-print("hello " + name)
+# Home Work progressia
+# item_1 = input("Please enter first digit: ")
+# item_2 = input("Please enter second digit: ")
+# item_N = input("Please enter N code: ")
+#
+# d_tarber = int(item_2) - int(item_1)
+# ancver = int(item_1) + d_tarber * (int(item_N) - 1)
+# print(ancver)
+
+# Home Work DAR
+# year = int(input("Please enter the year: "))
+# dar = (year - 1) // 100
+# print(dar + 1)
+
