bad830e126c9e38a219d8ee81686a5b0ad498c4e
avdsm <aravoskan@gmail.com>
2022-02-10 16:44:31 +0400

    Lesson 2

--- class-2.py
+++ class-2.py
@@ -8,7 +8,7 @@
 # # Old version
 # print("Hello {}! Next year you will be {} years old.".format(name, age + 1))
 
-# String functions
+# # String functions
 # full_name = "ara voskanyan"
 # print(full_name.title())
 # print(full_name)
@@ -16,11 +16,43 @@
 # print(full_name.upper())
 # print(full_name.lower())
 
-full_name = input("Please enter you fill name: ")
-print((full_name.lower()).title())
+# full_name = input("Please enter you fill name: ")
+# print((full_name.lower()).title())
+#
+# # String langth
+# print(len(full_name))
+# print(full_name.center(32, "="))
+# print(full_name.rjust(32, "."))
+# print(full_name.ljust(32, "."))
+#
+# # Տառերի քանակը տրված բառում / տարբերում է մեծատառը և փոքրքտքռը
+# print(full_name.count("a"))
+# print(type(full_name))  # type for variables
 
-# String langth
-print(len(full_name))
-print(full_name.center(32, "~"))
+# # Indexing
+full_name = "Ara Voskanyan"
+# print(full_name[1])
+# # # Slicing
+# print(full_name[2:6])
+# ## Start, Stop, Step
+# print(full_name[2:11:2])  ## 2-ից 10 տարածքում գտնվող նիշերի ամեն 2-րդ նիշը
+# ## Եթե սկսվում է 0-ից, առաջին թիվը կարելի է չգրել։ Նույնը արվում է նաև վերջի համար
+# print(full_name[:6])
+# print(full_name[3:])
+# ## Վերջին նիշը ընտրելը
+# print(full_name[-1])
+# print(full_name[9:2:-1]) # ## Թարս հերթականությամբ
+# print(full_name[::-1]) # Revers
+# print(full_name[-9:-2])
+
+# ### SubString
+print(full_name.find("a"))
+print(full_name.find("a", 3)) # ## 3-րդից հետո կսկսի փնտրել
+print(full_name.find("z"))  # ## Եթե չկա, ապա արդյունքը -1 է
+print(full_name.index("a"))
+print(full_name.index("a", 3))
+print(full_name.index("z"))  # ## Եթե չկա, ապա index() արդյունքը error է
+
+
 
 
