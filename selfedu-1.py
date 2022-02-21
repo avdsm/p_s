40406474b7cf0063bd87f3762a9c945d69182f06
avdsm <aravoskan@gmail.com>
2022-02-21 15:53:45 +0400

    После решения задач 21-02-22

--- selfedu-1.py
+++ selfedu-1.py
@@ -1,19 +1,28 @@
+# Задание к уроку Selfedu Python 3 #7:
+# строки - сравнения, срезы строк, базовые функции str, len, ord, in
+
 slovo = input('Вводите строку для поиска: ')
 sup_st = input("Водите символы, которые следует удалить: ")
-dlina = int(len(slovo))
-print(f'Длина строки сосотовляет {dlina} символов!')
-rezult_slovo = ''
-i = 0
+# Считаем количество вхожденрй подстроки "sup_st" в строку "slovo"
+counter = slovo.count(sup_st)
+
 if slovo.find(sup_st) != -1:
+    # Заменяем в строке символы подстроки на пустые символы и записываем в переменной "rezult_slovo"
     rezult_slovo = slovo.replace(sup_st, '')
-    i = 1
-    for sup_st in rezult_slovo:
-        i += 1
-        rezult_slovo = slovo.replace(sup_st, '')
+    print('\n' f'Длина заданой строки состовляет {len(slovo)} символа.' '\n' f'"{sup_st}" в строке встречался {counter} раз. После удаления получили строку "{rezult_slovo}".')
 else:
-    print(f'Символы "{sup_st}" в строке "{slovo}" не найдены!')
+    print('\n'f'Символы "{sup_st}" в строке "{slovo}" не найдены!')
+# Копирум слово в обратно порядке
+slovo_rev = slovo[::-1]
+# проверяем полиндромоне слово
+if slovo == slovo_rev:
+    print(f'Строка "{slovo}", ЕСТЬ полиндромоне слово.')
+else:
+    print(f'Строка "{slovo}", НЕ полиндромоне слово.')
 
-
-print()
-print(f'''Символы "{sup_st}" в строке "{slovo}" найдены "{i}" раз. 
-После удаления получили строку "{rezult_slovo}".''')
+# ######################
+print('##########################')
+predlozhenie = input('Вводите предложение, которе надо разбивать на слова! ')
+result_1 = predlozhenie.split(' ')
+for i in range(len(result_1)):
+    print(result_1[i])
